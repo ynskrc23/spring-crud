7a163361aff8305254b263d5829f360e4d1fb611
Yunus Karaca <ynskrc82@gmail.com>
2022-12-12 23:55:17 +0300

    spring boot validate işlemleri yapıldı

--- src/main/java/com/springcrud/springcrud/controller/CustomerController.java
+++ src/main/java/com/springcrud/springcrud/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,7 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping("/save")
-    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer){
+    public ResponseEntity<Customer> addCustomer(@Valid @RequestBody Customer customer){
         Customer addCustomer = customerService.addCustomer(customer);
         return new ResponseEntity<Customer>(addCustomer, HttpStatus.CREATED);
     }
--- src/main/java/com/springcrud/springcrud/entity/Customer.java
+++ src/main/java/com/springcrud/springcrud/entity/Customer.java
@@ -5,6 +5,10 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "customer")
@@ -17,8 +21,15 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty(message = "Bu alan boş geçilemez")
+    @Size(max = 255, message = "En fazla 255 karakter yazılabilir")
     private String name;
+
+    @NotEmpty(message = "Bu alan boş geçilemez")
+    @Size(max = 255, message = "En fazla 255 karakter yazılabilir")
     private String surname;
+
+    @Email(message = "Email formatına uygun değil")
     private String email;
 
 }
